ed3f765c6f3721e21eb5f726fea6bf0de3c54894
Venice Vargas <venicevargas1@live.com>
2020-09-29 22:12:59 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,11 @@
+while true
+puts "Never pass a school bus if it has flashing red lights"
+end
+
+loop do
+puts "Never pass a school bus if it has flashing red lights"
+end
+
+until false
+puts "Never pass a school bus if it has flashing red lights"
+end
---
+++ practice
@@ -0,0 +1,5 @@
+def hamburger(toppings)
+  toppings.collect do |topping|
+    "I love #{topping} on my burgers"
+  end
+end
